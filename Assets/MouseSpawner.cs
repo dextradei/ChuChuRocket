a701dbd2a7d282cec1dd4cdf12a461ddd4541ad7
Brendan Batchelder <dextradei@yahoo.com>
2016-12-30 13:01:20 -0500

    Minor changes

--- Assets/MouseSpawner.cs
+++ Assets/MouseSpawner.cs
@@ -17,8 +17,10 @@ public class MouseSpawner : MonoBehaviour {
 	void Start () {
         board = BoardObject.GetComponent<Board>();
         startTime = Time.time;
+        //Make sure we're centered
         position = new Vector3(Mathf.Round(transform.localPosition.x), Mathf.Round(transform.localPosition.y), 0f);
-	}
+        transform.localPosition = position;
+    }
 	
 	// Update is called once per frame
 	void Update () {
