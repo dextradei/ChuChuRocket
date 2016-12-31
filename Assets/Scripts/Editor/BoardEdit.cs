c9703628b9e0524c751b74f35c9c5a30faef9214
dextradei <dextradei@yahoo.com>
2016-12-30 23:35:13 -0500

    more cleanup and add comments

--- Assets/Scripts/Editor/BoardEdit.cs
+++ Assets/Scripts/Editor/BoardEdit.cs
@@ -5,17 +5,17 @@ using UnityEditor;
 
 [CustomEditor(typeof(Board))]
 public class BoardEdit : Editor {
-	SerializedProperty HeightProp;
-	SerializedProperty WidthProp;
-	SerializedProperty HorizontalWallsProp;
-	SerializedProperty VerticalWallsProp;
+	SerializedProperty heightProp;
+	SerializedProperty widthProp;
+	SerializedProperty horizontalWallsProp;
+	SerializedProperty verticalWallsProp;
 
 	void OnEnable()
 	{
-		WidthProp = serializedObject.FindProperty("Width");
-		HeightProp = serializedObject.FindProperty("Height");
-		HorizontalWallsProp = serializedObject.FindProperty("HorizontalWalls");
-		VerticalWallsProp = serializedObject.FindProperty("VerticalWalls");
+		widthProp = serializedObject.FindProperty("Width");
+		heightProp = serializedObject.FindProperty("Height");
+		horizontalWallsProp = serializedObject.FindProperty("HorizontalWalls");
+		verticalWallsProp = serializedObject.FindProperty("VerticalWalls");
 	}
 	public override void OnInspectorGUI()
 	{
@@ -30,25 +30,25 @@ public class BoardEdit : Editor {
 		//TODO: auto resizing of board graphics, currently static image of 16x16 squares
 		EditorGUILayout.BeginVertical();
 
-		int Width = WidthProp.intValue;
-		int Height = HeightProp.intValue;
+		int Width = widthProp.intValue;
+		int Height = heightProp.intValue;
 
-		WidthProp.intValue = EditorGUILayout.IntField("Width", Width);
-		HeightProp.intValue = EditorGUILayout.IntField("Height", Height);
+		widthProp.intValue = EditorGUILayout.IntField("Width", Width);
+		heightProp.intValue = EditorGUILayout.IntField("Height", Height);
 
 		EditorGUILayout.EndVertical();
 
 		//if the size changed and the new size is valid, resize the arrays
-		if (((WidthProp.intValue != Width) || (HeightProp.intValue != Height)) && (WidthProp.intValue > 0) && (HeightProp.intValue > 0))
+		if (((widthProp.intValue != Width) || (heightProp.intValue != Height)) && (widthProp.intValue > 0) && (heightProp.intValue > 0))
 		{
 			//resize HorizontalWalls and VerticalWalls arrays
 			Debug.Log("Resizing array");
-			Width = WidthProp.intValue;
-			Height = HeightProp.intValue;
+			Width = widthProp.intValue;
+			Height = heightProp.intValue;
 			//HorizontalWalls array is taller than the board by 1.  If we're keeping track of the wall below each square, need an extra row for the top row of walls
-			HorizontalWallsProp.arraySize = Width * (Height + 1);
+			horizontalWallsProp.arraySize = Width * (Height + 1);
 			//VerticalWalls array is wider than the board by 1.
-			VerticalWallsProp.arraySize = (Width + 1) * Height;
+			verticalWallsProp.arraySize = (Width + 1) * Height;
 
 		}
 
@@ -59,7 +59,7 @@ public class BoardEdit : Editor {
 		EditorGUILayout.BeginVertical();
 		for (int y = 0; y < Height + 1; y++)
 		{
-			//Horizontal Row
+			//Horizontal Walls Row
 			//Make the width slightly smaller
 			EditorGUILayout.BeginHorizontal();
 			EditorGUILayout.Space();
@@ -68,7 +68,7 @@ public class BoardEdit : Editor {
 			{
 				int i = ((Height - y) * Width) + x;
 				//Debug.Log("x: " + x + ", y: " + y + ", i: " + i);
-				SerializedProperty prop = HorizontalWallsProp.GetArrayElementAtIndex(i);
+				SerializedProperty prop = horizontalWallsProp.GetArrayElementAtIndex(i);
 				prop.boolValue = EditorGUILayout.Toggle(prop.boolValue);
 			}
 			EditorGUILayout.Space();
@@ -79,13 +79,13 @@ public class BoardEdit : Editor {
 			//skip the vertical row on the last loop
 			if (y > Height - 1) continue;
 
-			//Vertical Row
+			//Vertical Walls Row
 			EditorGUILayout.BeginHorizontal();
 			//Vertical rows are wider by one
 			for (int x = 0; x < Width + 1; x++)
 			{
 				int i = ((Height - y - 1) * (Width + 1)) + x;
-				SerializedProperty prop = VerticalWallsProp.GetArrayElementAtIndex(i);
+				SerializedProperty prop = verticalWallsProp.GetArrayElementAtIndex(i);
 				prop.boolValue = EditorGUILayout.Toggle(prop.boolValue);
 			}
 			EditorGUILayout.EndHorizontal();
