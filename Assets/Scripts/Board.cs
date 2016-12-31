356ed17ef3645ac4be4c9e988a8219e2b97262f1
dextradei <dextradei@yahoo.com>
2016-12-30 23:25:38 -0500

    cleanup and fix tabs

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -3,125 +3,123 @@ using System.Collections.Generic;
 using System.Collections;
 
 public class Board : MonoBehaviour {
-    [HideInInspector]
-	public bool[] HorizontalWalls;
-    [HideInInspector]
-    public bool[] VerticalWalls;
-
-    public int MouseLimit;
-
-	public GameObject HorizontalWall;
-	public GameObject VerticalWall;
-
-    [HideInInspector]
+	//These four have a custom editor which displays width/height boxes and a grid of checkboxes in the Inspector
+	[HideInInspector]
 	public int Width = 16;
-    [HideInInspector]
+	[HideInInspector]
 	public int Height = 16;
+	//for block (x,y), is there a horizontal wall blocking the south?
+	[HideInInspector]
+	public bool[] HorizontalWalls;
+	//for block (x,y), is there a vertical wall blocking the west?
+	[HideInInspector]
+	public bool[] VerticalWalls;
 
-    public GameObject MousePrefab;
+	public GameObject horizontalWallPrefab;
+	public GameObject verticalWallPrefab;
 
-    [System.NonSerialized]
-    private int mouseCount;
+	public GameObject mousePrefab;
 
-    [System.NonSerialized]
-    private Dictionary<int, MouseTrap> mouseTraps = new Dictionary<int, MouseTrap>();
+	//maximum number of mice before AddMouse() will stop adding and become a no-op
+	public int mouseLimit;
+	private int mouseCount;
+
+	//Keep an index of (x,y) position -> MouseTrap so we can quickly get the MouseTrap object at (x,y) if there is one
+	private Dictionary<int, MouseTrap> mouseTraps = new Dictionary<int, MouseTrap>();
 
 	// Use this for initialization
 	void Start () {
-        mouseCount = 0;
-        //Build Horizontal Walls
-        for(int y = 0; y < Height + 1; y++)
-        {
-            for(int x = 0; x < Width; x++)
-            {
-                int index = (y * (Width)) + x;
-                if (HorizontalWalls[index])
-                {
-                    GameObject wall = Instantiate(HorizontalWall, transform.parent);
-                    wall.transform.localPosition = new Vector3((float)x, (float)y, 0f);
-                }
-            }
-        }
-        //Build Vertical Walls
-        for(int y = 0; y < Height; y++)
-        {
-            for(int x = 0; x < Width + 1; x++)
-            {
-                int index = (y * (Width + 1)) + x;
-                if (VerticalWalls[index])
-                {
-                    GameObject wall = Instantiate(VerticalWall, transform.parent);
-                    wall.transform.localPosition = new Vector3((float)x, (float)y, 0f);
-                }
-            }
-        }
-        //Index Mouse Traps
-        MouseTrap[] traps = transform.parent.GetComponentsInChildren<MouseTrap>();
-        foreach (MouseTrap trap in traps)
-        {
-            int index = (Mathf.RoundToInt(trap.position.y) * Width) + Mathf.RoundToInt(trap.position.x);
-            mouseTraps.Add(index, trap);
-        }
-
-        //Test
-        /*
-        AddMouse(2, 2, Direction.Up);
-        AddMouse(2, 3, Direction.Right);
-        AddMouse(5, 5, Direction.Left);
-        AddMouse(10, 4, Direction.Down);
-        */
+		mouseCount = 0;
+		//Build Horizontal Walls
+		for(int y = 0; y < Height + 1; y++)
+		{
+			for(int x = 0; x < Width; x++)
+			{
+				int index = (y * (Width)) + x;
+				if (HorizontalWalls[index])
+				{
+					GameObject wall = Instantiate(horizontalWallPrefab, transform.parent);
+					wall.transform.localPosition = new Vector3((float)x, (float)y, 0f);
+				}
+			}
+		}
+		//Build Vertical Walls
+		for(int y = 0; y < Height; y++)
+		{
+			for(int x = 0; x < Width + 1; x++)
+			{
+				int index = (y * (Width + 1)) + x;
+				if (VerticalWalls[index])
+				{
+					GameObject wall = Instantiate(verticalWallPrefab, transform.parent);
+					wall.transform.localPosition = new Vector3((float)x, (float)y, 0f);
+				}
+			}
+		}
+		//Index MouseTraps
+		MouseTrap[] traps = transform.parent.GetComponentsInChildren<MouseTrap>();
+		foreach (MouseTrap trap in traps)
+		{
+			int index = (Mathf.RoundToInt(trap.position.y) * Width) + Mathf.RoundToInt(trap.position.x);
+			mouseTraps.Add(index, trap);
+		}
+		
 	}
 
-    public MouseTrap GetTrap(int x, int y)
-    {
-        MouseTrap ret;
-        int index = (y * Width) + x;
-        if (!mouseTraps.TryGetValue(index, out ret))
-            ret = null;
-        return ret;
-    }
+	//Get the MouseTrap at position (x,y) or return null if there isn't one
+	public MouseTrap GetTrap(int x, int y)
+	{
+		MouseTrap ret;
+		int index = (y * Width) + x;
+		if (!mouseTraps.TryGetValue(index, out ret))
+			ret = null;
+		return ret;
+	}
 
-    public void AddMouse(int x, int y, Direction direction)
-    {
-        if (mouseCount >= MouseLimit)
-            return;
-        GameObject o = Instantiate(MousePrefab, transform.parent);
-        o.transform.localPosition = new Vector3((float)x, (float)y, 0f);
-        o.GetComponent<Mouse>().direction = direction;
-        mouseCount++;
-    }
+	//Create a mouse at (x,y) that moves direction
+	public void AddMouse(int x, int y, Direction direction)
+	{
+		if (mouseCount >= mouseLimit)
+			return;
+		GameObject o = Instantiate(mousePrefab, transform.parent);
+		o.transform.localPosition = new Vector3((float)x, (float)y, 0f);
+		o.GetComponent<Mouse>().direction = direction;
+		mouseCount++;
+	}
 
-    public void RemoveMouse(GameObject mouse)
-    {
-        Destroy(mouse);
-        mouseCount--;
-    }
+	//Remove a mouse
+	public void RemoveMouse(GameObject mouse)
+	{
+		Destroy(mouse);
+		mouseCount--;
+	}
 
-    public bool CanGo(int x, int y, Direction direction)
-    {
-        bool ret = true;
-        //add 1 for up and right
-        switch (direction)
-        {
-            case Direction.Up:
-                if (HorizontalWalls[((y + 1) * Width) + x])
-                    ret = false;
-                break;
-            case Direction.Right:
-                if (VerticalWalls[(y * (Width + 1)) + (x + 1)])
-                    ret = false;
-                break;
-            case Direction.Down:
-                if (HorizontalWalls[(y * Width) + x])
-                    ret = false;
-                break;
-            case Direction.Left:
-                if (VerticalWalls[(y * (Width + 1)) + x])
-                    ret = false;
-                break;
-        }
-        return ret;
-    }
+	//Can a mouse at (x,y) move direction?
+	public bool CanGo(int x, int y, Direction direction)
+	{
+		bool ret = true;
+		//add 1 for up and right
+		switch (direction)
+		{
+			case Direction.Up:
+				if (HorizontalWalls[((y + 1) * Width) + x])
+					ret = false;
+				break;
+			case Direction.Right:
+				if (VerticalWalls[(y * (Width + 1)) + (x + 1)])
+					ret = false;
+				break;
+			case Direction.Down:
+				if (HorizontalWalls[(y * Width) + x])
+					ret = false;
+				break;
+			case Direction.Left:
+				if (VerticalWalls[(y * (Width + 1)) + x])
+					ret = false;
+				break;
+		}
+		return ret;
+	}
 	
 	// Update is called once per frame
 	void Update () {
--- Assets/Scripts/MouseTrap.cs
+++ Assets/Scripts/MouseTrap.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class MouseTrap : MonoBehaviour {
 
-    [System.NonSerialized]
-    public Vector3 position;
+	[System.NonSerialized]
+	public Vector3 position;
 
 	// Use this for initialization
 	void Start () {
-        //make sure we're centered
-        position = new Vector3(Mathf.Round(transform.localPosition.x), Mathf.Round(transform.localPosition.y), 0f);
-        transform.localPosition = position;
-    }
+		//make sure we're centered
+		position = new Vector3(Mathf.Round(transform.localPosition.x), Mathf.Round(transform.localPosition.y), 0f);
+		transform.localPosition = position;
+	}
 	
 	// Update is called once per frame
 	void Update () {
