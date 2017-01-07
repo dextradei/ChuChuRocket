164a653367037de968e6bbe04be93f63a90efc41
Lunaetic <lunaetic@lunaecy.com>
2017-01-07 02:23:59 -0500

    Fleshing out RoundTimer

--- Assets/Scripts/RoundTimerController.cs
+++ Assets/Scripts/RoundTimerController.cs
@@ -7,13 +7,38 @@ public class RoundTimerController : MonoBehaviour {
 
 	public GameObject countdown;
 
-	// Use this for initialization
+	public float delayTime;
+	public float roundTime;
+
+	public string goMessage;
+
+	private float delayLeft;
+	private float roundLeft;
+
 	void Start () {
-		countdown.GetComponent<Text>().text = "3…";
+		delayLeft = delayTime + 1;
+		roundLeft = roundTime;
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		countdown.GetComponent<Text>().text = "2…";
+		if (delayLeft > 0) {
+			delayLeft -= Time.deltaTime;
+
+			int integerDelayLeft = ((int) delayLeft);
+
+			if (integerDelayLeft == 0) {
+				countdown.GetComponent<Text>().text = goMessage;
+			} else {
+				countdown.GetComponent<Text>().text = ((int) delayLeft).ToString();
+			}
+
+		} else if (roundLeft > 0) {
+			roundLeft -= Time.deltaTime;
+			countdown.GetComponent<Text>().text = roundLeft.ToString().Substring(0,4);
+			
+		} else {
+			delayLeft = delayTime + 1;
+			roundLeft = roundTime;
+		}
 	}
 }
