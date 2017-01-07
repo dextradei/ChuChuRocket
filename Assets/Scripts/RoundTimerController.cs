9c1a26d9febb4433f5af5c6c36ade901181e0562
Lunaetic <lunaetic@lunaecy.com>
2017-01-07 01:31:11 -0500

    Basic objects for a countdown round timer

---
+++ Assets/Scripts/RoundTimerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoundTimerController : MonoBehaviour {
+
+	public GameObject countdown;
+
+	// Use this for initialization
+	void Start () {
+		countdown.GetComponent<Text>().text = "3…";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		countdown.GetComponent<Text>().text = "2…";
+	}
+}
