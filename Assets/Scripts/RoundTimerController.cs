89fe587333775c765944ff34dc04647f1d45ef8e
Lunaetic <lunaetic@lunaecy.com>
2017-01-07 02:33:10 -0500

    DRY

--- Assets/Scripts/RoundTimerController.cs
+++ Assets/Scripts/RoundTimerController.cs
@@ -14,31 +14,37 @@ public class RoundTimerController : MonoBehaviour {
 
 	private float delayLeft;
 	private float roundLeft;
+	private Text cachedText;
 
 	void Start () {
-		delayLeft = delayTime + 1;
-		roundLeft = roundTime;
+		cachedText = countdown.GetComponent<Text>();
+		InitTimes();
 	}
 
 	void Update () {
 		if (delayLeft > 0) {
 			delayLeft -= Time.deltaTime;
 
-			int integerDelayLeft = ((int) delayLeft);
+			int secondsLeft = ((int) delayLeft);
 
-			if (integerDelayLeft == 0) {
-				countdown.GetComponent<Text>().text = goMessage;
+			if (secondsLeft == 0) {
+				cachedText.text = goMessage;
+
 			} else {
-				countdown.GetComponent<Text>().text = ((int) delayLeft).ToString();
+				cachedText.text = ((int) delayLeft).ToString();
 			}
 
 		} else if (roundLeft > 0) {
 			roundLeft -= Time.deltaTime;
-			countdown.GetComponent<Text>().text = roundLeft.ToString().Substring(0,4);
+			cachedText.text = roundLeft.ToString().Substring(0, 4);
 			
 		} else {
-			delayLeft = delayTime + 1;
-			roundLeft = roundTime;
+			InitTimes();
 		}
+	}
+
+	void InitTimes() {
+		delayLeft = delayTime + 1;
+		roundLeft = roundTime;
 	}
 }
