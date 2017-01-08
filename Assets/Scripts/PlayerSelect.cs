7785ea6f2df11c7a77d45b4df9264d2701bc156c
dextradei <dextradei@yahoo.com>
2017-01-08 18:31:11 -0500

    New images, working on controller detection

---
+++ Assets/Scripts/PlayerSelect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSelect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		string[] gamepads = Input.GetJoystickNames();
+		Debug.Log("Number of Gamepads: " + gamepads.Length);
+		foreach (string name in gamepads)
+		{
+			Debug.Log(name);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
