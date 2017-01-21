07cc09f1530104803c1211f24138038b46cbd570
lunaetic <lunaetic@lunaecy.com>
2017-01-21 17:57:50 -0500

    Main Menu start

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+
+	public void NextScene ()
+	{
+		Debug.Log ("NextScene");
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+
+	public void Quit ()
+	{
+		Debug.Log ("NextScene");
+		Application.Quit ();
+	}
+}
